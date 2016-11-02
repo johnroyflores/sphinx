1e6277142b802356cf1c3a73d1c92fa37244840f
John Flores <johnflores@Johns-MacBook-Pro.local>
2016-11-02 00:00:24 -0500

    Here's literally everything. I really should've commited more.

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// $(function(){
+//     var queryURL = "https://api.instagram.com/oembed?url=http://instagr.am/p/BJwLbkVDmCz/";
+
+//     $.ajax({url: queryURL, method: 'GET'})
+//      .done(function(response) {
+//          var results = response;
+//            console.log(results);
+//     }); 
+
+//  });
